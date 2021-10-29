48e8adee3cd775c2fc93755dc131c630e7e74c17
Juan Diego Vallejo <juadival@espol.edu.ec>
2021-10-29 13:25:50 -0500

    Creation of classes CarInsurance and Customer

---
+++ ec.edu.espol.workshops/lib/src/main/java/ec/edu/espol/workshops/Customer.java
@@ -0,0 +1,10 @@
+package ec.edu.espol.workshops;
+
+public class Customer {
+	private int age;
+    private String sex;
+    private boolean isMarried;
+    private boolean hasdrivingLicense;
+    private int base;
+
+}
