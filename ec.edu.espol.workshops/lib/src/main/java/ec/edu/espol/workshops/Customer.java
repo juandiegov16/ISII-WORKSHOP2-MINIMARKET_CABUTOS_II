dd748a5f55474d7df9976058bcf104463a945cdc
Alexander Alzate <alex.alzate95@gmail.com>
2021-12-12 01:21:41 -0500

    Change premium to 600 (Jenkins)

--- ec.edu.espol.workshops/lib/src/main/java/ec/edu/espol/workshops/Customer.java
+++ ec.edu.espol.workshops/lib/src/main/java/ec/edu/espol/workshops/Customer.java
@@ -15,7 +15,7 @@ public class Customer {
         this.sex = sex;
         this.isMarried = estadoMarital;
         this.hasdrivingLicense = licencia;
-        this.base = 500;
+        this.base = 600;
     }
 
     
